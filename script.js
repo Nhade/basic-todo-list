3961df41c87069f6d5f14f1080ab830fb6688c75
Nhade <shademinecraft21@gmail.com>
2025-01-28 16:54:31 +0800

    Add checkbox persistence
    Tasks are now handled with objects.
    Execution flow may require some improvement.

--- script.js
+++ script.js
@@ -9,18 +9,23 @@ function updateTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasksArray));
 }
 
-function createTask(taskName) {
+function createTask(taskObj) {
     const newTaskListItem = document.createElement("li");
     const newTaskCheckbox = document.createElement("input");
     const newTaskName = document.createElement("span");
     const newTaskButton = document.createElement("button");
 
+    newTaskName.textContent = taskObj.name;
     newTaskCheckbox.type = "checkbox";
-    newTaskName.textContent = taskName;
+    newTaskCheckbox.checked = taskObj.done;
+    newTaskCheckbox.addEventListener("change", ()=>{
+        taskObj.done = newTaskCheckbox.checked;
+        updateTasks();
+    });
     newTaskButton.textContent = "Delete";
     newTaskButton.addEventListener("click", ()=>{
         newTaskButton.parentElement.remove();
-        tasksArray.splice(tasksArray.indexOf(taskName), 1);
+        tasksArray.splice(tasksArray.map((e) => e.name).indexOf(taskObj.name), 1);
         updateTasks();
     });
     
@@ -32,15 +37,20 @@ function createTask(taskName) {
     taskList.appendChild(newTaskListItem);
 }
 
-tasksArray.forEach((taskName)=>{
-    createTask(taskName);
+tasksArray.forEach((taskObj)=>{
+    createTask(taskObj);
 });
 
 taskCreateButton.addEventListener("click", ()=>{
     const name = taskNameInput.value;
-    taskNameInput.value = '';
-    createTask(name);
-    tasksArray.push(name);
-    updateTasks();
+    if(tasksArray.includes(name)){
+        
+    }else{
+        taskNameInput.value = '';
+        const taskObj = {name: name, done: false};
+        createTask(taskObj);
+        tasksArray.push(taskObj);
+        updateTasks();
+    }
 });
 
